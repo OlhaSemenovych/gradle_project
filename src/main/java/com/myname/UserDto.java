857c179df6544beb5e5db42457d53addbb28650e
olha <oh.semenovych@gmail.com>
2023-06-09 00:37:58 +0300

    complete gradle settings

---
+++ src/main/java/com/myname/UserDto.java
@@ -0,0 +1,24 @@
+package com.myname;
+
+public class UserDto {
+
+    String name;
+    String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
